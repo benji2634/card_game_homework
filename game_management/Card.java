493a4132486d824c15add90f0e4e36181594e325
Tom Benham <benji26@hotmail.co.uk>
2016-11-05 21:18:30 +0000

    added throw exception for suit but not able to work out value - commented out

--- game_management/Card.java
+++ game_management/Card.java
@@ -6,26 +6,28 @@ public class Card {
   private ValueType valueType;
   private SuitType suit;
 
-  public Card(ValueType valueType, SuitType suit) {
-    this.valueType = valueType;
-    this.suit = suit;
-  }
+  public Card(ValueType theValue, SuitType theSuit) {
+   if (theSuit != SuitType.SPADES && theSuit != SuitType.HEARTS && theSuit != SuitType.DIAMONDS && theSuit != SuitType.CLUBS) throw new IllegalArgumentException("Illegal playing card suit");
+   // if (theValue < 1 || theValue > 11) throw new IllegalArgumentException("Illegal playing card value");
+   this.valueType = theValue;
+   this.suit = theSuit;
+ }
 
   public ValueType getValueType() {
     return valueType;
   }
 
-  // public void setValueType(ValueType valueType) {
-  //     this.valueType = valueType;
-  //   }
+  public void setValueType(ValueType valueType) {
+      this.valueType = valueType;
+    }
 
   public SuitType getSuitType() {
     return suit;
   }
 
-  // public void setSuitType(SuitType suit) {
-  //   this.suit = suit;
-  // }
+  public void setSuitType(SuitType suit) {
+    this.suit = suit;
+  }
   
   public String toString() {
     return this.valueType + " of " + this.suit;
